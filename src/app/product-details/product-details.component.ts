da65a9c678da07cd2856494f6c4389689df7f5df
Obodo David <obododavidsop@gmail.com>
2023-03-04 09:17:08 +0100

    Updated Product details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,28 +1,50 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductsService } from '../services/products.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
+import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent implements OnInit {
 
-  @Input() product = new Product;
-  id!: number;
+export class ProductDetailsComponent implements OnInit {
+  product = new Product;
   quantity: string = "1";
+  count: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+  id!: number;
   
   constructor (
     private service: ProductsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private cartService: CartService
   ) {}
 
   ngOnInit() {
-  this.route.params.subscribe((params: Params) => {
-    this.id = params['id']
-  })}
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id']
+    })
+
+    this.service.getProducts().subscribe(res => {
+      for (let i = 0; i < res.length; i++) {
+        if (res[i].id == this.id) {
+          this.product = res[i];
+          break;
+        }
+      }
+    });
+  }
+
+  updateProduct(val: string) {
+    this.quantity = val;
+  }
+
+  addToCart(cartItem: Product): void {
+    cartItem['quantity'] = parseInt(this.quantity);
+    this.cartService.addToCart(cartItem);
+  }
 
   
 }
